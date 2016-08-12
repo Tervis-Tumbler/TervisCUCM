2c0117644c69e6138efa348e1965424735ebe3ef
fhoseinih <hosseini@tervis.com>
2016-08-12 15:50:36 -0400

    Create functions Invoke-TervisCUCMTerminateUser and Get-CUCMDeviceName

--- TervisCUCM.psm1
+++ TervisCUCM.psm1
@@ -1,9 +1,32 @@
-﻿Function Remove-TervisCUCMUser {
+﻿#Requires -Modules CUCMPowerShell
+
+Function Invoke-TervisCUCMTerminateUser {
     param(
         $UserName
     )
 
-    $QueryResult = Get-CUCMDeviceName -UserIDAssociatedWithDevice $UserName
-    $Result = Remove-CUCMPhone -Name $QueryResult.name
-    Set-CUCMLine -DN 
+    $CUCMUser = Get-CUCMUser -UserID $UserName
+    $DeviceNames = $CUCMUser.associatedDevices.device
+    $Phones = foreach ($DeviceName in $DeviceNames) { Get-CUCMPhone -Name $DeviceName }
+    $Lines = $Phones.lines.line
+
+    ForEach ($DirectoryNumber in $Lines.dirn ) { 
+        $SetCUCMLineResponse = Set-CUCMLine -Pattern $DirectoryNumber.Pattern -RoutePartitionName $DirectoryNumber.routePartitionName."#text" -Description "" -AlertingName "" -AsciiAlertingName "" 
+    }
+
+    $RemoveCUCMPhoneResponse = $Phones | Remove-CUCMPhone
+}
+
+function Get-CUCMDeviceName {
+    param(
+        [Parameter(Mandatory)][String]$UserIDAssociatedWithDevice
+    )
+
+    $QueryForDevicesByUserID = @"
+select device.name, enduser.userid from device, enduser, enduserdevicemap
+where device.pkid=enduserdevicemap.fkdevice and  
+enduser.pkid=enduserdevicemap.fkenduser and enduser.userid = '$UserIDAssociatedWithDevice'
+"@
+
+    Invoke-CUCMSQLQuery -SQL $QueryForDevicesByUserID
 }
