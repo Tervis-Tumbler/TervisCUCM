e218ae7fb904dbf0c1c1988bf9070096b92b2ec3
ChrisMagnuson <hourglasssand@gmail.com>
2016-08-12 11:27:18 -0400

    Intial commit funciton Remove-TervisCUCMUser

---
+++ TervisCUCM.psm1
@@ -0,0 +1,9 @@
+﻿Function Remove-TervisCUCMUser {
+    param(
+        $UserName
+    )
+
+    $QueryResult = Get-CUCMDeviceName -UserIDAssociatedWithDevice $UserName
+    $Result = Remove-CUCMPhone -Name $QueryResult.name
+    Set-CUCMLine -DN 
+}
