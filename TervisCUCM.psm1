74318f18e93fd92762a2680d246b312613b3a0be
fhoseinih <hosseini@tervis.com>
2016-08-30 15:53:09 -0400

    Add functions Find-CUCMUserWithoutMatchingTelephoneNumberInAD, Invoke-TervisCUCMTerminateUserWithoutUserInCUCM, and make $UserName mandatory

--- TervisCUCM.psm1
+++ TervisCUCM.psm1
@@ -1,8 +1,46 @@
 ﻿#Requires -Modules CUCMPowerShell
 
+function Find-CUCMUserWithoutMatchingTelephoneNumberInAD {
+    $CUCMUsers = Find-CUCMUser -firstName "%"
+    foreach ($CUCMUserID in $CUCMUsers.userID) {
+        $CUCMUser = Get-CUCMUser -UserID $CUCMUserID
+        $DeviceNames = $CUCMUser.associatedDevices.device
+        if ($DeviceNames) {
+            $Phones = foreach ($DeviceName in $DeviceNames) { Get-CUCMPhone -Name $DeviceName }
+            $DirectoryNumbers = $Phones.lines.line.dirn.pattern
+            if ($DirectoryNumbers) {
+                $ADUser = Get-ADUser -Identity $CUCMUserID -properties TelephoneNumber
+                if ($ADUser.TelephoneNumber -notin $DirectoryNumbers) {
+                    $ADUser 
+                }
+            }
+        }
+    }
+}
+
+Function Invoke-TervisCUCMTerminateUserWithoutUserInCUCM {
+    param(
+        [Parameter(Mandatory)]$PhoneName
+    )
+
+    $PhoneSearchResults = Find-CUCMPhone -Name $PhoneName
+    $DeviceNames = $PhoneSearchResults.name
+    $Phones = foreach ($DeviceName in $DeviceNames) { Get-CUCMPhone -Name $DeviceName }
+    $Lines = $Phones.lines.line
+
+    ForEach ($DirectoryNumber in $Lines.dirn ) { 
+        $SetCUCMLineResponse = Set-CUCMLine -Pattern $DirectoryNumber.Pattern -RoutePartitionName $DirectoryNumber.routePartitionName."#text" -Description "" -AlertingName "" -AsciiAlertingName ""
+
+    }
+  
+   $RemoveCUCMPhoneResponse = $Phones | Remove-CUCMPhone
+   $RemoveCUCMPhoneResponse
+}
+    
+
 Function Invoke-TervisCUCMTerminateUser {
     param(
-        $UserName
+        [Parameter(Mandatory)]$UserName
     )
 
     $CUCMUser = Get-CUCMUser -UserID $UserName
@@ -15,6 +53,7 @@ Function Invoke-TervisCUCMTerminateUser {
     }
 
     $RemoveCUCMPhoneResponse = $Phones | Remove-CUCMPhone
+    $RemoveCUCMPhoneResponse
 }
 
 function Get-CUCMDeviceName {
